73cd702d30efb89eb3bb64f1cd7f3f4b6fb7c39f
Alex <alexmillerprojects@gmail.com>
2020-06-08 15:02:34 -0700

    finished the stack file in the module project

--- queue/queue.py
+++ queue/queue.py
@@ -14,15 +14,43 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 class Queue:
-    def __init__(self):
-        self.size = 0
-        # self.storage = ?
-    
-    def __len__(self):
-        pass
+    class Node:
+        def __init__(self, element, _next):
+            # initiate the Node for the stack that will hold the current and next element
+            self.element = element
+            self._next = _next
 
-    def enqueue(self, value):
-        pass
+    def __init__(self): #initialize the Queue class that will manage the values of the head (first item in the queue) tail (newest or latest item added to the queue)
+        self.size = 0
+        self.head = None
+        self.tail = None
+    
+    def __len__(self): #override the len function to return the size of the queue instantiated in the class
+        return self.size
+
+    def is_empty(self): #checks the current length of the queue. This value is changed when elements or queued or dequeued
+        return self.size == 0
+
+    def enqueue(self, element): 
+        new = self.Node(element, None) #creates a new element to be queued based in input value
+        if self.is_empty(): #checks if queue is empty, if it is, it will set the head of the queue to the input element
+            self.head = new
+        else: #if queue has length, then it will add to the tail, (or the back of the queue)
+            self.tail._next = new
+        self.tail = new
+        self.size += 1 
 
     def dequeue(self):
-        pass
+        if self.is_empty(): #checks if queue is empty, if it is, this function will not be able to dequeue anything
+            return None
+        result = self.head.element #if it has length, it will set the result of the first item to be dequeued
+        self.head = self.head._next #then it will set the head as the following element after the result
+        self.size -= 1 #it will account for the dequeued element
+        if self.is_empty():  #if after dequeueing the element, the queue length is 0, it will set the tail to none and then return the current result, being the item dequeued
+            self.tail = None
+        return result
+
+    def first(self): #this is a method that returns the first item in the queue
+        if self.is_empty():
+            return f"Queue is empty."
+        return self.head.element
--- stack/stack.py
+++ stack/stack.py
@@ -13,33 +13,33 @@ return elements in Last In First Out order.
 
 class Stack:
     class Node:
-        def __init__(self, element, _next):
+        def __init__(self, element, _next): #instantiate the node class that will navigate the data structure of the stack
             self.element = element
             self._next = _next
 
-    def __init__(self):
+    def __init__(self): #initialioze the stack class
         self.head = None
         self.size = 0
 
-    def __len__(self):
+    def __len__(self): #this method overrides the len function to just simple return the size accounted for by the stack class
         return self.size
 
-    def is_empty(self):
+    def is_empty(self): #method checks if the stack is empty
         return self.size == 0
 
-    def push(self, element):
+    def push(self, element): #method pushes on the new element to the stack based on the input
         self.head = self.Node(element, self.head)
-        self.size += 1
+        self.size += 1 #adds to the size counter every time this method is called, or any element is added to the stack
 
     def pop(self):
-        if self.is_empty():
+        if self.is_empty(): #if the stack is empty, there is nothing to pop
             return None
-        result = self.head.element
-        self.head = self.head._next
-        self.size -= 1
-        return result
+        result = self.head.element #sets the first element of the stack to be removed to the result
+        self.head = self.head._next #sets up the next element in the stack after the result is popped
+        self.size -= 1 #accounts for the update in the stack size
+        return result #returns the result
 
-    def top(self):
+    def top(self): #this method returns the top element in the stack (or the last)
         if self.is_empty():
             return None
         return self.head.element
